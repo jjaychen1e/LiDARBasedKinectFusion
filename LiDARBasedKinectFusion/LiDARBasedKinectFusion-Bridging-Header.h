790a4ef6178b6c7b0d787b1bce107998ee95686e
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2021-01-06 16:01:39 +0800

    Initial Commit

---
+++ LiDARBasedKinectFusion/LiDARBasedKinectFusion-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  LiDARBasedKinectFusion-Bridging-Header.h
+//  LiDARBasedKinectFusion
+//
+//  Created by 陈俊杰 on 2021/1/6.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
